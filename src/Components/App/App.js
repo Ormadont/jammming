6729daede2c793aaab05ef31e35061cb2e4c7831
Ormadont <evgeniy1demyanov@gmail.com>
2018-05-28 06:46:09 +0400

    some commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,24 @@ import Playlist from '../Playlist/Playlist';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchResults:[
+      {name: 'Moon',
+      artist: 'Mikle Jackson',
+      album: 'Platinum album',
+      id: '009801'},
+      {name: 'Apple',
+      artist: 'Bob Rick',
+      album: 'Gold album',
+      id: '009802'},
+      {name: 'Soul',
+      artist: 'Don Valent',
+      album: 'Silver album',
+      id: '009803'},
+    ]};
+  }
+
   render() {
     return (
       <div>
@@ -12,7 +30,7 @@ class App extends Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults}/>
             <Playlist />
           </div>
         </div>
